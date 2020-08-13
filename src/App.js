56ecc435ddf8e5afe304ec0f6feed6250512dd0b
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-08-13 16:37:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	state = {
+		name: "Gilbert"
+	};
+
+	logInfo() {
+		console.log(this);
+	}
+
+	componentDidMount() {
+		this.logInfo();
+	}
+
+	// Render is a lifecycle method
+	// Only required lifecycle method as it is needed to render component to browser
+	// This render component returns JSX. Looks like HTML, but it isn't. Looks a lot like Vue.
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>Voici l'application débutant de React.</p>
+					<h3>Hello, my name is {this.state.name}</h3>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer">
+						Learn React
+					</a>
+				</header>
+			</div>
+		);
+	}
 }
-
-export default App;
