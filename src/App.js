61f7756cf4b0b66327043888d0d15ae72608bbe6
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-08-13 16:59:03 -0500

    component basics. 2 eeezee

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Todos from "./components/Todos";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -15,24 +16,12 @@ export default class App extends Component {
 		this.logInfo();
 	}
 
-	// Render is a lifecycle method
-	// Only required lifecycle method as it is needed to render component to browser
-	// This render component returns JSX. Looks like HTML, but it isn't. Looks a lot like Vue.
 	render() {
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>Voici l'application débutant de React.</p>
-					<h3>Hello, my name is {this.state.name}</h3>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer">
-						Learn React
-					</a>
-				</header>
+				<img src={logo} className="App-logo" alt="logo" />
+				<Todos />
+				<h2>I will make more components in this branch</h2>
 			</div>
 		);
 	}
