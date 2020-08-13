8bbd215f2f36ba9262b73055a35964597647f90e
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-08-13 18:15:31 -0500

    proptypes. nice to know react has it and it is pree simple

--- src/components/CurrentTodo.js
+++ src/components/CurrentTodo.js
@@ -1,9 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class CurrentTodo extends React.Component {
 	render() {
 		return <h4>{this.props.titleProp}</h4>;
 	}
 }
+
+// Look at Todos.js for useful comments on prop-types and how to use them
+
+CurrentTodo.propTypes = {
+	titleProp: PropTypes.string.isRequired
+};
 
 export default CurrentTodo;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,9 @@
 import React from "react";
 import CurrentTodo from "./CurrentTodo";
+import PropTypes from "prop-types";
 
 class Todos extends React.Component {
 	render() {
-		console.log(this.props.todosProp);
 		return this.props.todosProp.map((currentTodo) => {
 			// NEW: My favourite thing to do with JS frameworks. Vue, React, Svelte, all of them: rendering components through a loop
 			// key property is like v-bind:key in Vue
@@ -11,5 +11,20 @@ class Todos extends React.Component {
 		});
 	}
 }
+
+// We use propTypes to configure our props, similar to the props array or object in VueJS.
+// Don't forget to import it up top
+// Syntaxe (that I know of so far)
+/**
+ * {ComponentName}.propTypes = {
+ * 		{propName}: Proptypes.{typeOfProp}.isRequired
+ * }
+ * is required is like required: true in Vue, indicating we must have this prop in the component
+ */
+
+// Good practise for strictening your props
+Todos.propTypes = {
+	todosProp: PropTypes.array.isRequired
+};
 
 export default Todos;
