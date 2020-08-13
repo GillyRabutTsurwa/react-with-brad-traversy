25ac7e6da5d1541bb32feae1d1fc9c4c10e2a104
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-08-13 17:04:54 -0500

    state basics. Pree easey. My spelling is terribol

--- src/App.js
+++ src/App.js
@@ -4,24 +4,36 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default class App extends Component {
+	// NOTE: C'est comme "data" en Vue.
 	state = {
-		name: "Gilbert"
+		todos: [
+			{
+				id: 1,
+				title: "Take out the rubbish",
+				completed: false
+			},
+			{
+				id: 2,
+				title: "Buy groceries",
+				completed: false
+			},
+			{
+				id: 3,
+				title: "Fix the roof",
+				completed: false
+			}
+		]
 	};
 
-	logInfo() {
-		console.log(this);
-	}
-
-	componentDidMount() {
-		this.logInfo();
-	}
-
 	render() {
+		//NOTE: I get it. the return section is for JSX only.
+		// stuff inside the render and outside the return is where you can do console.log(), declare variables for your state, etc etc etc.
+		console.log(this.state.todos);
 		return (
 			<div className="App">
 				<img src={logo} className="App-logo" alt="logo" />
 				<Todos />
-				<h2>I will make more components in this branch</h2>
+				<h2>Play around with state if needed</h2>
 			</div>
 		);
 	}
