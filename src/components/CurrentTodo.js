f85422d168b6cce5ef1e3a0086ae4a3e068b1d2e
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-08-20 22:12:59 -0500

    will finish tomorrow. im' tired

--- src/components/CurrentTodo.js
+++ src/components/CurrentTodo.js
@@ -2,7 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class CurrentTodo extends React.Component {
-	//NEW: Voici ma meilleure méthode. Utiliser une méthod que l'on va appeler dans parmi notre code JSX
+	constructor(props) {
+		super(props);
+		this.markComplete = this.markComplete.bind(this);
+	}
+
 	getStyle() {
 		const styleObj = {
 			backgroundColor: "rgba(40, 44, 52, 0.3)",
@@ -13,8 +17,28 @@ class CurrentTodo extends React.Component {
 		return styleObj;
 	}
 
+	//NEW: Method for the change checkbox event
+	//markComplete(e) {
+	//console.log(this); // prints out undefined
+	//console.log(this.props); // prints an error that reads along the lines of "can't read property props of undefined"
+	// NOTE:This is because when it comes for events, the method does not know what this is. It doesn't point to the component. To solve this, we need to bind our method to the React Component Object..
+	//NOTE: This function will be commented out and recreated below. Read these notes if you need to refresh.
+	//}
+
+	markComplete(e) {
+		console.log(this);
+		this.props.todoProp.completed = !this.props.todoProp.completed;
+	}
+
 	render() {
-		return <h4 style={this.getStyle()}>{this.props.todoProp.title}</h4>;
+		return (
+			<div style={this.getStyle()}>
+				<p>
+					<input type="checkbox" onChange={this.markComplete} />
+					{this.props.todoProp.title}
+				</p>
+			</div>
+		);
 	}
 }
 
