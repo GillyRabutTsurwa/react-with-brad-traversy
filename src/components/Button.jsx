db800038e6a83bd0131e7193b69709ff1ce16c0b
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-05-25 12:56:39 -0400

    5.1. Adding Events to Elements - pretty straightfoward - just like in Vue & Svelte, you can access the element DOM node object

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,16 @@
 import { PropTypes } from "prop-types";
 
+const cliquer = (e) => {
+    const { target } = e;
+    console.log("cliqué");
+    console.log(e);
+    console.log(target);
+};
+
 function Button(props) {
-    const { text, colour = "crimson" } = props;
+    const { text = "Add", colour = "crimson" } = props;
     return (
-        <button style={{ backgroundColor: colour }} className="btn">
+        <button onClick={cliquer} style={{ backgroundColor: colour }} className="btn">
             {text}
         </button>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,9 @@
 import Button from "./Button";
 function Header(props) {
-    const subHeaderStyles = {
-        color: "gold",
-        textTransform: "uppercase",
-    };
-
     return (
         <header className="header">
             <h1>{props.title}</h1>
-            <Button text="Hello" colour="green" />
-            <Button text="Hola" colour="lightcoral" />
-            <Button text="Salut" colour="cornflowerblue" />
-            <Button text="Yo" />
+            <Button />
         </header>
     );
 }
